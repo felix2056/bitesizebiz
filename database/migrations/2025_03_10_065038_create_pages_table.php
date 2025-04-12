365114408bb035d76f853076af36de07da65cf7f
CodeBreaker <felixdaniel2056@gmail.com>
2025-04-12 17:03:41 +0100

    chore: made some changes

--- database/migrations/2025_03_10_065038_create_pages_table.php
+++ database/migrations/2025_03_10_065038_create_pages_table.php
@@ -22,26 +22,8 @@ return new class extends Migration
             $table->string('author')->nullable()->default('Dev Shah');
             $table->string('author_link')->nullable()->default('/author/devshah');
             $table->string('author_image_url')->nullable()->default('/images/devshah.jpg');
-            $table->text('author_description')->nullable()->default('I’m an entrepreneurial student diving into the exciting world of small business acquisitions for under $25k on Acquire.com. At 18, I’m building a thriving side hustle in the sub-$25k space and developing an Ed-tech startup—check out eleven59.ai. I’m also launching Sourcely.net, an AI tool for students, and aiming for $10k MRR. Join me as I navigate the small biz landscape.');
-            $table->json('author_social_links')->nullable()->default('
-            [
-                {
-                    "name": "Facebook",
-                    "url": "https://www.facebook.com/profile.php?id=100016824821262"
-                },
-                {
-                    "name": "Twitter",
-                    "url": "https://twitter.com/devlikesbizness"
-                },
-                {
-                    "name": "LinkedIn",
-                    "url": "https://www.linkedin.com/in/devlikesbizness/"
-                },
-                {
-                    "name": "Instagram",
-                    "url": "https://www.instagram.com/devlikesbizness/"
-                }
-            ]');
+            $table->text('author_description')->nullable();
+            $table->json('author_social_links')->nullable();
             $table->integer('views')->default(0);
             $table->timestamps();
             $table->softDeletes();
--- database/seeders/PageSeeder.php
+++ database/seeders/PageSeeder.php
@@ -20,6 +20,28 @@ class PageSeeder extends Seeder
             'excerpt' => 'This is the home page. It is the main page of the website. It is the first page that people see when they visit the website.',
             'content' => 'Home',
             'url' => '/',
+            'author' => 'Dev Shah',
+            'author_link' => '/author/devshah',
+            'author_image_url' => '/images/devshah.jpg',
+            'author_description' => 'I’m an entrepreneurial student diving into the exciting world of small business acquisitions for under $25k on Acquire.com. At 18, I’m building a thriving side hustle in the sub-$25k space and developing an Ed-tech startup—check out eleven59.ai. I’m also launching Sourcely.net, an AI tool for students, and aiming for $10k MRR. Join me as I navigate the small biz landscape.',
+            'author_social_links' => '[
+                {
+                    "name": "Facebook",
+                    "url": "https://www.facebook.com/profile.php?id=100016824821262"
+                },
+                {
+                    "name": "Twitter",
+                    "url": "https://twitter.com/devlikesbizness"
+                },
+                {
+                    "name": "LinkedIn",
+                    "url": "https://www.linkedin.com/in/devlikesbizness/"
+                },
+                {
+                    "name": "Instagram",
+                    "url": "https://www.instagram.com/devlikesbizness/"
+                }
+            ]',
         ]);
 
         Page::create([
@@ -28,6 +50,28 @@ class PageSeeder extends Seeder
             'excerpt' => 'This is the about page. It is the page that tells people about the website.',
             'content' => 'About',
             'url' => '/about',
+            'author' => 'Dev Shah',
+            'author_link' => '/author/devshah',
+            'author_image_url' => '/images/devshah.jpg',
+            'author_description' => 'I’m an entrepreneurial student diving into the exciting world of small business acquisitions for under $25k on Acquire.com. At 18, I’m building a thriving side hustle in the sub-$25k space and developing an Ed-tech startup—check out eleven59.ai. I’m also launching Sourcely.net, an AI tool for students, and aiming for $10k MRR. Join me as I navigate the small biz landscape.',
+            'author_social_links' => '[
+                {
+                    "name": "Facebook",
+                    "url": "https://www.facebook.com/profile.php?id=100016824821262"
+                },
+                {
+                    "name": "Twitter",
+                    "url": "https://twitter.com/devlikesbizness"
+                },
+                {
+                    "name": "LinkedIn",
+                    "url": "https://www.linkedin.com/in/devlikesbizness/"
+                },
+                {
+                    "name": "Instagram",
+                    "url": "https://www.instagram.com/devlikesbizness/"
+                }
+            ]',
         ]);
 
         Page::create([
@@ -36,6 +80,28 @@ class PageSeeder extends Seeder
             'excerpt' => 'This is the contact page. It is the page that allows people to contact the website.',
             'content' => 'Contact',
             'url' => '/contact',
+            'author' => 'Dev Shah',
+            'author_link' => '/author/devshah',
+            'author_image_url' => '/images/devshah.jpg',
+            'author_description' => 'I’m an entrepreneurial student diving into the exciting world of small business acquisitions for under $25k on Acquire.com. At 18, I’m building a thriving side hustle in the sub-$25k space and developing an Ed-tech startup—check out eleven59.ai. I’m also launching Sourcely.net, an AI tool for students, and aiming for $10k MRR. Join me as I navigate the small biz landscape.',
+            'author_social_links' => '[
+                {
+                    "name": "Facebook",
+                    "url": "https://www.facebook.com/profile.php?id=100016824821262"
+                },
+                {
+                    "name": "Twitter",
+                    "url": "https://twitter.com/devlikesbizness"
+                },
+                {
+                    "name": "LinkedIn",
+                    "url": "https://www.linkedin.com/in/devlikesbizness/"
+                },
+                {
+                    "name": "Instagram",
+                    "url": "https://www.instagram.com/devlikesbizness/"
+                }
+            ]',
         ]);
 
         Page::create([
@@ -44,6 +110,28 @@ class PageSeeder extends Seeder
             'excerpt' => 'This is the privacy policy page. It is the page that tells people about the privacy policy of the website.',
             'content' => 'Privacy Policy',
             'url' => '/privacy-policy',
+            'author' => 'Dev Shah',
+            'author_link' => '/author/devshah',
+            'author_image_url' => '/images/devshah.jpg',
+            'author_description' => 'I’m an entrepreneurial student diving into the exciting world of small business acquisitions for under $25k on Acquire.com. At 18, I’m building a thriving side hustle in the sub-$25k space and developing an Ed-tech startup—check out eleven59.ai. I’m also launching Sourcely.net, an AI tool for students, and aiming for $10k MRR. Join me as I navigate the small biz landscape.',
+            'author_social_links' => '[
+                {
+                    "name": "Facebook",
+                    "url": "https://www.facebook.com/profile.php?id=100016824821262"
+                },
+                {
+                    "name": "Twitter",
+                    "url": "https://twitter.com/devlikesbizness"
+                },
+                {
+                    "name": "LinkedIn",
+                    "url": "https://www.linkedin.com/in/devlikesbizness/"
+                },
+                {
+                    "name": "Instagram",
+                    "url": "https://www.instagram.com/devlikesbizness/"
+                }
+            ]',
         ]);
 
         Page::create([
@@ -52,6 +140,28 @@ class PageSeeder extends Seeder
             'excerpt' => 'This is the terms of service page. It is the page that tells people about the terms of service of the website.',
             'content' => 'Terms of Service',
             'url' => '/terms-of-service',
+            'author' => 'Dev Shah',
+            'author_link' => '/author/devshah',
+            'author_image_url' => '/images/devshah.jpg',
+            'author_description' => 'I’m an entrepreneurial student diving into the exciting world of small business acquisitions for under $25k on Acquire.com. At 18, I’m building a thriving side hustle in the sub-$25k space and developing an Ed-tech startup—check out eleven59.ai. I’m also launching Sourcely.net, an AI tool for students, and aiming for $10k MRR. Join me as I navigate the small biz landscape.',
+            'author_social_links' => '[
+                {
+                    "name": "Facebook",
+                    "url": "https://www.facebook.com/profile.php?id=100016824821262"
+                },
+                {
+                    "name": "Twitter",
+                    "url": "https://twitter.com/devlikesbizness"
+                },
+                {
+                    "name": "LinkedIn",
+                    "url": "https://www.linkedin.com/in/devlikesbizness/"
+                },
+                {
+                    "name": "Instagram",
+                    "url": "https://www.instagram.com/devlikesbizness/"
+                }
+            ]',
         ]);
     }
 }
